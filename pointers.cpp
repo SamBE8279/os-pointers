ba362f719a4058941dea40873d98c73a6297f69f
Sam <sam@Ubuntu-20.04-VM>
2021-02-11 23:40:12 -0600

    Commit

--- pointers.cpp
+++ pointers.cpp
@@ -21,25 +21,27 @@ int main(int argc, char **argv)
     Student student;
     double average;
 
-    // Sequence of user input -> store in fields of `student`
-    // student.id = promptInt("Please enter the student's id number: ", 0 ,999999999);
-    // std::cout << student.id << "\n";
+    //Sequence of user input -> store in fields of `student`
+    student.id = promptInt("Please enter the student's id number: ", 0 ,999999999);
 
-    // std::cout << "Please enter the student's first name: ";
-    // char first_name[128];
-    // fgets(first_name, 127, stdin);
-    // student.f_name = first_name;
+    std::cout << "Please enter the student's first name: ";
+    char first_name[128];
+    scanf("%s", first_name);
+    student.f_name = first_name;
 
-    // std::cout << "Please enter the student's last name: ";
-    // char last_name[128];
-    // fgets(last_name, 127, stdin);
-    // student.l_name = last_name;
+    std::cout << "Please enter the student's last name: ";
+    char last_name[128];
+    scanf("%s", last_name);
+    student.l_name = last_name;
+
+    std::cin.ignore(1); //The consequences of using scanf.
 
     student.n_assignments = promptInt("Please enter how many assignments were graded: ", 0 ,134217728);
-    std::cout << student.n_assignments << "\n";
 
+    std::cout << "\n";
     double grades[student.n_assignments];
     for(int i=0; i<student.n_assignments; i++) {
+        //Format message for "promptDouble()" into a single string.
         std::string message0 = "Please enter grade for assignment ";
         std::string message1 = std::to_string(i);
         std::string message2 = ": ";
@@ -48,17 +50,14 @@ int main(int argc, char **argv)
         grades[i] = promptDouble(message, 0, 100);
     }
     student.grades = grades;
-
-    average = student.n_assignments;
+    
+    average = student.n_assignments;    //Temporarily set the average to the number of assignments so the size of the grades array is known in "calculateStudentAverage()"
     calculateStudentAverage(student.grades, &average);
     
-    std::cout << "Student: " << student.f_name << " " << student.l_name << " [" << student.id << "]";
+    std::cout << "Student: " << student.f_name << " " << student.l_name << " [" << student.id << "]\n";
     std::cout << std::fixed;
     std::cout << std::setprecision(1);
     std::cout << "  Average grade: " << average;
-
-    // Call `CalculateStudentAverage(???, ???)`
-    // Output `average`
 
     return 0;
 }
@@ -76,22 +75,22 @@ int promptInt(std::string message, int min, int max) {
     do {
         valid_input = true;
         std::cout << message;
-        getline (std::cin, user_input);
-        //Try to convert user's input to an integer. If the input starts with any characters, an exception will be thrown and caught and the user will be prompted again.
-        try{
-            value = std::stoi(user_input);
-        } catch(...) {
+        getline(std::cin, user_input);
+        //Manually goes through the user's string checking for anything that isn't a number.
+        for(int i=0; i<user_input.length(); i++) {
+            if(user_input[i] < 48 || user_input[i] > 57 ) {
+                std::cout << "Sorry, I cannot understand your answer\n";
+                valid_input = false;
+                break;
+            }
+        }
+        //The above test does not catch an empty input array so this IF does.
+        if(user_input.length() == 0) {
             std::cout << "Sorry, I cannot understand your answer\n";
             valid_input = false;
         }
-        //If the input starts with numbers but includes characters, no exception will be thrown even though the input is invalid. The conversion to an int will succeed but
-        //this int will be different than user's string. Therefore, the converted int and the user's string can be compared as strings to determine if the user should be prompted again.
         if(valid_input) {
-            std::string conversion_output = std::to_string(value);
-            if(conversion_output.compare(user_input) != 0) {
-                std::cout << "Sorry, I cannot understand your answer\n";
-                valid_input = false;
-            }
+            value = std::stoi(user_input);
             if(value < min || value > max) {
                 std::cout << "Sorry, I cannot understand your answer\n";
                 valid_input = false;
@@ -107,8 +106,7 @@ int promptInt(std::string message, int min, int max) {
    min: minimum value to accept as a valid double
    max: maximum value to accept as a valid double
 */
-double promptDouble(std::string message, double min, double max)
-{
+double promptDouble(std::string message, double min, double max) {
     double value = 0;
     std::string user_input;
     bool valid_input = true;
@@ -129,6 +127,11 @@ double promptDouble(std::string message, double min, double max)
                 break;
             }
         }
+        //The above test does not catch an empty input array so this IF does. It also catches the case of a single "." being entered.
+        if(user_input.length() == 0 || (periods_seen == 1 && user_input[1] == 0)) {
+            std::cout << "Sorry, I cannot understand your answer\n";
+            valid_input = false;
+        }
         if(valid_input) {
             value = std::stod(user_input);  //Converts the string to a double.
             if(value < min || value > max) {
@@ -145,10 +148,9 @@ double promptDouble(std::string message, double min, double max)
    object: pointer to anything - your choice! (but choose something that will be helpful)
    avg: pointer to a double (can store a value here)
 */
-void calculateStudentAverage(void *object, double *avg)
-{
+void calculateStudentAverage(void *object, double *avg) {
     double *grades = (double*)object;
-    int assignments = *avg;
+    int assignments = *avg;   //Gets the size of "grades[]" from average
     double sum = 0.0;
     for(int i=0; i<assignments; i++) {
         sum += grades[i];
